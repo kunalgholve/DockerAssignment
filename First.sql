01b2bdff540b423c40b0b9d6f78ad16dfa7987a8
kunalgholve <45362598+kunalgholve@users.noreply.github.com>
2020-05-29 23:17:51 +0530

    Update First.sql

--- First.sql
+++ First.sql
@@ -7,6 +7,6 @@ drop table if exists user;
 
 CREATE TABLE IF NOT EXISTS user(id INT PRIMARY KEY,name VARCHAR(25) NOT NULL,age INT NOT NULL,department VARCHAR(25) NOT NULL,subject VARCHAR(25) NOT NULL);
 
-insert into user values(1,"shubham",24, "PUSCD", "dos");
+insert into user values(1,"kunal",20, "PUSCD", "dos");
 insert into user values(2,"pratik", 25, "Physics", "phy");
 insert into user values(3,"sagar", 26, "Math", "algebra");
